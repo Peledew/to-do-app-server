8fbda8b4e2c17008241c336cb2bbc8c2e6700422
Luka Petrovic <petrovicluka967@gmail.com>
2025-08-28 23:06:12 +0200

    User entity model

---
+++ ToDoAppServer/ToDoAppServer.Domain/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ToDoAppServer.Domain.Enums;
+
+namespace ToDoAppServer.Domain.Entities
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required] 
+        public required string LastName { get;set; }
+
+        [Required]
+        public required string Username { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(UserGender))]
+        public required UserGender Gender { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(UserRole))]
+        public required UserRole Role { get; set; }
+    }
+}
---
+++ ToDoAppServer/ToDoAppServer.Persistence/Database/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAppServer.Domain.Entities;
+
+namespace ToDoAppServer.Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Enum conversion
+            modelBuilder.Entity<User>()
+            .Property(u => u.Gender)
+            .HasConversion<string>();
+
+            modelBuilder.Entity<User>()
+            .Property(u => u.Role)
+            .HasConversion<string>();
+
+            //Unique constraints
+            modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+
+            modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
